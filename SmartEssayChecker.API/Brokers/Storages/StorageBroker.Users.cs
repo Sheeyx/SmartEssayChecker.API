82184e17475a3cc0808033ac5519bf0146aa110c
Sheeyx <sheeyx00@gmail.com>
2023-12-14 15:42:22 +0900

    BROKERS: ADD User Storage

---
+++ SmartEssayChecker.API/Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SmartEssayChecker.API.Models.Users;
+
+namespace SmartEssayChecker.API.Brokers.Storages;
+
+public partial class StorageBroker
+{ 
+    public DbSet<User> Users { get; set; }
+    
+    public async ValueTask<User> InsertUserAsync(User user) =>
+        await InsertAsync(user);
+
+    public IQueryable<User> SelectAllUsers() =>
+        SelectAll<User>().AsQueryable();
+
+    public async ValueTask<User> SelectUserByIdAsync(Guid userId) =>
+        await SelectAsync<User>(userId);
+
+    public async ValueTask<User> UpdateUserAsync(User user) =>
+        await UpdateAsync(user);
+
+    public async ValueTask<User> DeleteUserAsync(User user) =>
+        await DeleteAsync(user);
+}
